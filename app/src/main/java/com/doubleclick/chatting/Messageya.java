b788efd6a4eb9365b9a82a747cc547ac2b24eaf6
eslam <eslamfgazy600@gmail.com>
2022-04-16 17:49:59 +0200

    Chatting

---
+++ app/src/main/java/com/doubleclick/chatting/Messageya.java
@@ -0,0 +1,25 @@
+package com.doubleclick.chatting;
+ 
+import android.app.Application;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.OkHttp3Downloader;
+import com.squareup.picasso.Picasso;
+
+public class Messageya extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //offline
+        FirebaseDatabase.getInstance("https://chatting-a6254-default-rtdb.firebaseio.com/").setPersistenceEnabled(true);
+
+        /*Picasso*/
+        Picasso.Builder builder = new Picasso.Builder(this);
+        builder.downloader(new OkHttp3Downloader(this,Integer.MAX_VALUE));
+        Picasso built =builder.build();
+        built.setIndicatorsEnabled(true);
+        built.setLoggingEnabled(true);
+        Picasso.setSingletonInstance(built);
+
+    }
+}
